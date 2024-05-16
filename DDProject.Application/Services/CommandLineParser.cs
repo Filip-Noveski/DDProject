048877c3af49d83042a10e79419246571b7afd5e
Filip Noveski <filip.noveski@cse.uist.edu.mk>
2024-05-16 21:05:40 +0200

    CommandLineParser service [Application]

---
+++ DDProject.Application/Services/CommandLineParser.cs
@@ -0,0 +1,42 @@
+﻿using DDProject.Application.Exceptions;
+using DDProject.P2P.Services;
+
+namespace DDProject.Application.Services;
+
+public class CommandLineParser
+{
+    private readonly P2PService _p2pService;
+
+    public CommandLineParser(P2PService p2pService)
+    {
+        _p2pService = p2pService;
+    }
+
+    private static Task WarnUnkownCommand(string command)
+    {
+        Console.WriteLine($"The command '{command}' was not recognised.");
+        return Task.CompletedTask;
+    }
+
+    private async Task PromptAndExecuteSend()
+    {
+        string targetUrl = Console.ReadLine()!;
+        int targetPort = int.Parse(Console.ReadLine()!);
+        string message = Console.ReadLine()!;
+        await _p2pService.Send(targetUrl, targetPort, message);
+    }
+
+    public async Task AwaitAndParseInput()
+    {
+        string op = Console.ReadLine()!;
+
+        Task t = op switch
+        {
+            "listen" => _p2pService.Listen(),
+            "send" => PromptAndExecuteSend(),
+            "exit" => throw new ShutdownApplicationException(),
+            _ => WarnUnkownCommand(op)
+        };
+        await t;
+    }
+}
